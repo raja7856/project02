ac10f1d0d50a658abc3986a6057649af311016a0
unknown <rp1999071@gmail.com>
2023-06-03 18:34:11 +0530

    basic website done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,9 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-public data = "hello world";
+  public productDetails : any;
+public getProductDetail(product: any){
+  console.log(product);
 
-public color = "text-primary"
+  this.productDetails = product;
+  
+}
+  public products =[
+    {
+      id: 1,
+      name: 'product 1',
+      price : '100',
+      description : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      imgUrl: 'assets/images/home/img.jpg',
+    },
+  
 
-public data1 = true;
+    {
+      id: 2,
+      name: 'product 2',
+      price : '200',
+      description : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      imgUrl: 'assets/images/home/img.jpg',
+      },
+
+    {
+      id: 3,
+      name: 'product 3',
+      price : '300',
+      description : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      imgUrl: 'assets/images/home/img.jpg',
+    },
+     
+    {
+      id: 4,
+      name: 'product 4',
+      price : '400',
+      description : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      imgUrl: 'assets/images/home/img.jpg',
+    },
+
+
+    {
+      id: 5,
+      name: 'product 5',
+      price : '500',
+      description : 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      imgUrl: 'assets/images/home/img.jpg',
+    },
+
+    
+  ]
 }
